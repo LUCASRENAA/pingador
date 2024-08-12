86ed72530fafe24831f66bcf965248b0f654ff47
Lucas Meira <lucasrenaa1996@gmail.com>
2024-08-11 21:22:27 -0300

    fazendo o ping no path

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+import os
 # Create your views here.
+
+def pingar(request,ip):
+    command = f'ping -c 1 {ip}'
+    os.system(command)
+    return render(request,'core/index.html',{'ip':ip})
